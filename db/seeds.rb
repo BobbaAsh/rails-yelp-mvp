3ab8153d141df70f96b86fd29a7ef76329b30fa8
Daniel Kaddous <danielkaddous.ys@gmail.com>
2019-05-22 14:38:12 +0200

    Rails YelpMVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '065842355',
+    category:       'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '065895645226',
+    category:       'italian'
+  },
+    {
+    name:         'Zimble',
+    address:      '86A High St, Peckham',
+    phone_number:  '062326426',
+    category:       'japanese'
+  },
+    {
+    name:         'Le France',
+    address:      '134 Rue Paradis',
+    phone_number:  '0658595926',
+    category:       'french'
+  },
+    {
+    name:         'Ze Frite',
+    address:      '98A Shoreditch , London E1 6PQ',
+    phone_number:  '065895645226',
+    category:       'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
